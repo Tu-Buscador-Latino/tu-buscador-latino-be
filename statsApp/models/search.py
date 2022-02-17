56b8b08be9834030780322c5f364fc14a6639d0c
Diego <idfernando3@gmail.com>
2022-02-17 17:16:48 -0500

    Search model updated

--- statsApp/models/search.py
+++ statsApp/models/search.py
@@ -6,7 +6,12 @@ class Search(models.Model):
 
     id = models.BigAutoField(primary_key=True)
     word = models.CharField(max_length = 120)
-    count = models.IntegerField()
-    start_date = models.DateTimeField()
+    count = models.IntegerField(default=1)
+    start_date = models.DateTimeField(default=timezone.now)
     last_date = models.DateTimeField(default=timezone.now)
     last_results = models.IntegerField()
+
+    def update_current(self, last_results):
+        self.count = self.count + 1
+        self.last_date = timezone.now()
+        self.last_results = last_results
